ca2a7cbba4d5b762c7cf61cc62366922d3ae15e6
tommykorch <tkorchmit@bk.ru>
2024-09-27 19:42:05 +0700

    Project v1

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication1
-
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val inputChar = inputField.text.toString().trim()
+
+        if (inputChar.length != 1 || !inputChar[0].isLowerCase() || !inputChar[0].isLetter()) {
+            resultTextView.text = "Введенный символ не является строчной латинской буквой."
+            return
+        }
+
+        // Проверка на гласные
+        when (inputChar[0]) {
+            'a', 'e', 'i', 'o', 'u' -> resultTextView.text = "Это гласные буквы."
+            else -> resultTextView.text = "Возможно, это согласные буквы."
+        }
     }
-}
+    }
