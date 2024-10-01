f45ae6ed77e1a6c47e49253372e34b08436d1fed
tommykorch <tkorchmit@bk.ru>
2024-10-01 18:42:02 +0700

    Project v3

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -20,7 +20,10 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val inputField = findViewById<EditText>(R.id.inputField)
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val resultTextView = findViewById<TextView>(R.id.resultTextViewTask1)
+        val inputField1 = findViewById<EditText>(R.id.inputField1)
+        val resultTextView1= findViewById<TextView>(R.id.resultTextViewTask2)
+
         //val inputChar = inputField.text.toString().trim()
         inputField.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -46,5 +49,36 @@ class MainActivity : AppCompatActivity() {
                 // Не нужно ничего делать после изменения текста
             }
         })
+        // Обработка ввода натурального числа n
+        inputField1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val nStr = s.toString().trim()
+                if (nStr.isNotEmpty()) {
+                    val n = nStr.toIntOrNull()
+                    if (n != null && n > 0) {
+                        val sum = calculateSum(n)
+                        resultTextView1.text = "Сумма S = $sum"
+                    } else {
+                        resultTextView1.text = "Введите натуральное число больше 0."
+                    }
+                } else {
+                    resultTextView1.text = ""
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+    }
+    private fun calculateSum(n: Int): Double {
+        var sum = 0.0
+        var factorial = 1.0 // Начальное значение для 0!
+        var count=0
+        for (i in 1..n) {
+            factorial *= i // Вычисляем i!
+            sum += 1 / factorial // Добавляем 1/i!
+        }
+
+        return sum
     }
 }
