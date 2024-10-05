cce2cc42e3a88fcc2f8ad1012f0806c056fb7c22
tommykorch <tkorchmit@bk.ru>
2024-10-05 12:20:27 +0700

    Project v5.1 Final

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -73,7 +73,6 @@ class MainActivity : AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
-
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
                 val inputString = s.toString()
